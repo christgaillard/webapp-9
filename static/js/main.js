50918653a818a56aa6f44ff5575d121e084af5de
cgai <gaillard@brocelia.fr>
2022-09-24 11:37:43 +0200

    corrige la lecture et affichage du json

--- static/js/main.js
+++ static/js/main.js
@@ -2,15 +2,15 @@ $(function(){
 	$('button').click(function(){
 		var user = $('#usr').val();
 		$.ajax({
-			url: 'https://openclassroom.azurewebsites.net/api/test',
+			url: 'https://serverlessopenclassroom.azurewebsites.net/api/test',
+			//url: 'http://localhost:7071/api/test',
 			//data: $('form').serialize(),
 			data:JSON.stringify($("form").serializeArray()),
 			type: 'POST',
 			success: function(response){
-				console.log(response);
-				const obj = JSON.parse(response);
-				$("#categ").html(obj[0].categories);
-                $("#livres").html(obj[0].recommendation);
+				test = JSON.parse(response);
+				console.log(test);
+                $("#livres").html("livre 1 : "+ test[0] + "<br>"+"livre 2 : "+ test[1]+ "<br>"+"livre 3 : "+ test[2] + "<br>"+"livre 4 : "+ test[3]+"<br>"+"livre 5 : "+ test[4]);
 			},
 			error: function(error){
 				console.log(error);
